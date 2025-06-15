e32e86872bb888b034046a92b61a0a941d73d917
Eric-McKinney <ericmckinney.md@gmail.com>
2025-06-15 16:42:15 -0400

    Fix timezone

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,7 @@
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  time.timeZone = "America/New-York";
+  time.timeZone = "America/New_York";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
